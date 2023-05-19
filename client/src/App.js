3417e3282c609d8a974f8ba0b24db08987a4680f
Kate Steed <sunfiresteed@yahoo.com>
2023-05-19 13:49:47 -0600

    added app and index.js

---
+++ client/src/App.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import Auth from "./utils/auth";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+  } from '@apollo/client';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+const httpLink = createHttpLink({
+uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+const token = localStorage.getItem('id_token');
+return {
+    headers: {
+    ...headers,
+    authorization: token ? `Bearer ${token}` : '',
+    },
+};
+});
+
+const client = new ApolloClient({
+link: authLink.concat(httpLink),
+cache: new InMemoryCache(),
+});
+
+function App() {
+
+   if (Auth.loggedIn()) {
+    return (
+        <ApolloProvider client={client}>
+        <Router>
+
+            <Routes>
+
+                <Route
+                    path='/'
+                    element={ <>
+                    <Nav/>
+                    <Home/>
+                    </> }
+                />
+
+                <Route
+                    path='/Login'
+                    element={ <>
+                    <Nav/>
+                    <Login/>
+                    </> }
+                />
+                <Route
+                    path='/Signup'
+                    element={ <>
+                      <Nav/>
+                      <Signup/>
+                      </> }
+                />
+            </Routes>
+        </Router>
+    </ApolloProvider>
+    )
+   } else {
+    return (
+        <ApolloProvider client={client}>
+        <Router>
+
+            <Routes>
+
+                <Route
+                    path='/'
+                    element={ <>
+                    <Nav/>
+                    <Landing/>
+                    </> }
+                />
+
+                <Route
+                    path='/Login'
+                    element={ <>
+                    <Nav/>
+                    <Login/>
+                    </> }
+                />
+                <Route
+                    path='/Signup'
+                    element={ <>
+                      <Nav/>
+                      <Signup/>
+                      </> }
+                />
+            </Routes>
+        </Router>
+    </ApolloProvider>
+    )
+   }
+};
+
+export default App;
