c198a7341f794d13ee7ac7b73db1a81ea3933822
Kate Steed <sunfiresteed@yahoo.com>
2023-06-05 15:03:49 -0600

    added login page html

---
+++ client/src/pages/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+function Login(props) {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: { email: formState.email, password: formState.password },
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (event) {
+            console.log(event);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    return (
+        <div className="container">
+            <h2> Login to Your Account</h2>
+            <form onSubmit={handleFormSubmit}>
+                <div className='flex-row space-between my-2'>
+                    <label htmlFor='email'>Email: </label>
+                    <input
+                        placeholder="cryptID@example.com"
+                        name='email'
+                        type='email'
+                        id='email'
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='flex-row space-between my-2'>
+                    <label htmlFor='pwd'>Password: </label>
+                    <input
+                        placeholder="********"
+                        name='password'
+                        type='password'
+                        id='password'
+                        onChange={handleChange}
+                    />
+                </div>
+                {error ? (
+                    <div>
+                        <p className='errorText'>Email or password is incorrect</p>
+                    </div> 
+                ) : null}
+                <div className='submitBtn'>
+                    <button type='submit'>Log In</button>
+                </div>
+            </form>
+            <Link to="/Signup">First Time? Sign up here!</Link>
+        </div>
+    );
+}
+
+
+export default Login;
